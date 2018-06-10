2452104599e0dff41414ead061a4194c8ff0a690
nickbeau <nick@hubone.com>
2018-06-10 13:13:31 +1000

    Added Blazor Ref Finder

---
+++ RefFinder/Database1/Referees.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Referees]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [emailAddress] NVARCHAR(MAX) NULL
+)
